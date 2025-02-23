d9305c732438d50e66ee24cfcd6ac161cf0e8c43
hsin yu <tina2793778@gmail.com>
2025-02-23 23:50:06 +0800

    feat: 🎸 新增 entities、routes

--- week5/app.js
+++ week5/app.js
@@ -5,6 +5,8 @@ const pinoHttp = require('pino-http')
 
 const logger = require('./utils/logger')('App')
 const creditPackageRouter = require('./routes/creditPackage')
+const skillRouter = require('./routes/skill')
+const userRouter = require('./routes/user')
 
 const app = express()
 app.use(cors())
@@ -26,6 +28,8 @@ app.get('/healthcheck', (req, res) => {
   res.send('OK')
 })
 app.use('/api/credit-package', creditPackageRouter)
+app.use('/api/skill', skillRouter)
+app.use('/api/user', userRouter)
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
