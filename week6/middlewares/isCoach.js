ad02f4abf07d39b6e930b5a30ef2afc542e4a67a
hsin yu <tina2793778@gmail.com>
2025-03-06 16:58:34 +0800

    feat: 🎸 調整 isCoach 驗證 middleware

--- week6/middlewares/isCoach.js
+++ week6/middlewares/isCoach.js
@@ -1,19 +1,23 @@
 const FORBIDDEN_MESSAGE = '使用者尚未成為教練';
 const PERMISSION_DENIED_STATUS_CODE = 401;
 
-function generateError(
-  status = PERMISSION_DENIED_STATUS_CODE,
-  message = FORBIDDEN_MESSAGE
-) {
+const generateError = (message) => {
   const error = new Error(message);
-  error.status = status;
+  error.status = PERMISSION_DENIED_STATUS_CODE;
   return error;
-}
+};
 
-module.exports = (req, res, next) => {
-  if (!req.user || req.user.role !== 'COACH') {
-    next(generateError());
-    return;
+module.exports = async (req, res, next) => {
+  try {
+    const { role } = req.user;
+    if (!role || role !== 'COACH') {
+      throw generateError(FORBIDDEN_MESSAGE);
+    }
+    next();
+  } catch (error) {
+    res.status(error.status || PERMISSION_DENIED_STATUS_CODE).json({
+      status: 'failed',
+      message: error.message || FORBIDDEN_MESSAGE,
+    });
   }
-  next();
 };
